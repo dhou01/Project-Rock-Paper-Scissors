deb5d7d0e4d075e728d6f1a747ba670ecd6350b9
dhou01 <17430711+dhou01@users.noreply.github.com>
2024-06-21 20:29:03 +0800

    Add computer choice function

--- myscript.js
+++ myscript.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+let randomNumber = Math.floor(Math.random() * 3);
+console.log(randomNumber);
+
+function getComputerChoice() {
+
+    if(randomNumber === 0) {
+        return 'Rock';
+    } else if(randomNumber === 1) {
+        return 'Paper';
+    } else {
+        return 'Scissors';
+    }
+}
+console.log(getComputerChoice());
+
+function getHumanChoice() {
+
+}
