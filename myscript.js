71f1fe448d53bd7b9b2b98867631d714e28090d8
dhou01 <17430711+dhou01@users.noreply.github.com>
2024-06-21 21:03:18 +0800

    Add human choice function

--- myscript.js
+++ myscript.js
@@ -1,18 +1,32 @@
 let randomNumber = Math.floor(Math.random() * 3);
 console.log(randomNumber);
 
-function getComputerChoice() {
+    function getComputerChoice() {
 
-    if(randomNumber === 0) {
-        return 'Rock';
-    } else if(randomNumber === 1) {
-        return 'Paper';
-    } else {
-        return 'Scissors';
+        if(randomNumber === 0) {
+            return 'Rock';
+        } else if(randomNumber === 1) {
+            return 'Paper';
+        } else {
+            return 'Scissors';
+        }
     }
-}
-console.log(getComputerChoice());
+    console.log(getComputerChoice());
 
-function getHumanChoice() {
 
-}
+
+let userOption = prompt("Choose: Rock, Paper or Scissors");
+
+    function getHumanChoice() {
+
+        if(userOption === 'Rock') {
+            return 'Rock';
+        } else if(userOption === 'Paper') {
+            return 'Paper';
+        } else if(userOption === 'Scissors') {
+            return 'Scissors';
+        } else {
+            return alert('Invalid choice, please try again');
+        }
+    }
+    console.log(getHumanChoice());
